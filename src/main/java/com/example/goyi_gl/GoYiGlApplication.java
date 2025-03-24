d21bd60b6fb94bf6356c581dbc878a5a4c8ed70d
thanhcanhquanca <chuanhquan89@gmail.com>
2025-03-24 19:16:10 +0700

    init repository

---
+++ src/main/java/com/example/goyi_gl/GoYiGlApplication.java
@@ -0,0 +1,13 @@
+package com.example.goyi_gl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoYiGlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoYiGlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/goyi_gl/GoYiGlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goyi_gl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoYiGlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
